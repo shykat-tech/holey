68fa4d34db29609cf18c738270a505dd3e63d646
Shykat <shykat@co.design>
2026-02-02 17:53:58 +0600

    Fix: lenis on mobile

--- app/plugins/lenis.client.js
+++ app/plugins/lenis.client.js
@@ -1,56 +1,33 @@
-import { defineNuxtPlugin } from "#app";
 import Lenis from "lenis";
 
 export default defineNuxtPlugin((nuxtApp) => {
     const router = useRouter();
 
-
-    // 🔒 Lock scroll only on homepage
-    if (import.meta.client) {
-        document.documentElement.style.overflow = "hidden";
-    }
-
     const isMobile =
-        typeof window !== "undefined" &&
+        import.meta.client &&
         ("ontouchstart" in window || navigator.maxTouchPoints > 0);
 
     const lenis = new Lenis({
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        smooth: true,
+        smooth: !isMobile,
+        smoothTouch: false,
         autoRaf: false,
-
-        // 🐢 Slower on mobile
-        duration: isMobile ? 2.8 : 1.2,
-        touchMultiplier: isMobile ? 0.45 : 1,
+        duration: isMobile ? 1 : 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        touchMultiplier: 1,
     });
 
     nuxtApp.provide("lenis", lenis);
 
-    let ready = false;
-
     function raf(time) {
         lenis.raf(time);
-
-        if (!ready) {
-            ready = true;
-        }
-
         requestAnimationFrame(raf);
     }
 
     requestAnimationFrame(raf);
 
-    // ✅ ALWAYS SCROLL TO TOP ON ROUTE CHANGE
     if (import.meta.client) {
         router.afterEach(() => {
-            // reset native scroll
-            window.scrollTo(0, 0);
-        });
-
-        window.addEventListener("load", () => {
-            setTimeout(() => {
-                lenis.scrollTo(0, { immediate: true });
-            }, 0);
+            lenis.scrollTo(0, {immediate: true});
         });
     }
 });
